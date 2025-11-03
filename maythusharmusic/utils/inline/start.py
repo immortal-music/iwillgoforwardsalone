1084bac71408ed813d1544db505a6bd5b4457b9f
immortal-music <emoji2292299@gmail.com>
2025-11-03 17:44:44 +0630

    Add files via upload

---
+++ maythusharmusic/utils/inline/start.py
@@ -0,0 +1,34 @@
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import WebAppInfo
+
+import config
+from maythusharmusic import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [InlineKeyboardButton(text=_["S_B_6"], web_app=WebAppInfo(url="https://maythusharmusic.site"))],
+        [
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+    ]
+    return buttons
